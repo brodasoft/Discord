e3c436dbb2fadeecb0429dad37ebb2df24bafcf9
Quered <74057307+Quered@users.noreply.github.com>
2024-02-01 14:25:18 +0100

    Zmiana nazwy serwera
    komenda: "!changeservername nazwa"

--- main.py
+++ main.py
@@ -18,22 +18,20 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-# Command to greet the user
-@bot.command(name='hello', help='Greet the user')
-async def hello(ctx):
-    await ctx.send(f'Hello {ctx.author.name}!')
+    # Change bot username
+    await bot.user.edit(username='JEJ')
 
-@bot.event 
+@bot.event
 async def on_message(message): 
     username = str(message.author).split("#")[0] 
     channel = str(message.channel.name) 
     user_message = str(message.content) 
-  
+
     print(f'Message {user_message} by {username} on {channel}') 
-  
+
     if message.author == bot.user: 
         return
-  
+
     if channel == "random": 
         if user_message.lower() == "hello" or user_message.lower() == "hi": 
             await message.channel.send(f'Hello {username}') 
@@ -48,6 +46,31 @@ async def on_message(message):
         elif "spadaj" in user_message.lower():
             await message.channel.send(f'Ty tez spadaj, {username}!')
             return
+        elif user_message.lower().startswith('!changeservername'):
+            await bot.process_commands(message)
+            return
+
+    await bot.process_commands(message)
+
+# Command to greet the user
+@bot.command(name='changeservername', help='Change the server name')
+async def change_server_name(ctx, new_name):
+    print('Command received!')
+    try:
+        print(f'Attempting to change server name to {new_name}')
+        if ctx.guild.me.guild_permissions.manage_guild:
+            await ctx.guild.edit(name=new_name)
+            print(f'Success! Server name changed to {new_name}')
+            await ctx.send(f'Server name changed to {new_name}')
+        else:
+            print('Bot does not have the "Manage Server" permission.')
+            await ctx.send('Bot does not have the "Manage Server" permission.')
+    except discord.errors.Forbidden:
+        print('Changing server name forbidden. Make sure the bot has "Manage Server" permission.')
+        await ctx.send('Changing server name forbidden. Make sure the bot has "Manage Server" permission.')
+    except discord.errors.HTTPException as e:
+        print(f'An error occurred: {e}')
+        await ctx.send(f'An error occurred while changing the server name: {e}. Please check the bot\'s permissions.')
 
 # Run the bot with your bot token
 token = os.getenv('TOKEN')
